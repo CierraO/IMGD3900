68b98f79a0705f6b6af404df7eda86520f0480b1
Cierra <cierra362@gmail.com>
2024-11-08 06:53:31 -0500

    added tileset and music

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,6 +8,7 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	start_game.grab_focus()
+	AudioPlayer.play()
 
 
 func _on_start_game_pressed() -> void:
