3a8b3ed9e65de8cd774f6be45f8728efc736f093
Cierra <cierra362@gmail.com>
2024-11-15 08:03:24 -0500

    adjused stats and magic skill

--- scripts/magic_attacks/lower_enemy_def.gd
+++ scripts/magic_attacks/lower_enemy_def.gd
@@ -5,7 +5,8 @@ func use(opponent_stats=null, self_stats=null, battle=null):
 	battle.display_text("You cast a basic spell." if (self_stats == battle.current_player_stats) else "%s casts a basic spell." % battle.enemy.name)
 	await(battle.textbox_closed)
 	
-	opponent_stats["def"] = max(0, opponent_stats["def"] - self_stats["mag"])
+	var dmg = randi_range(1, self_stats["mag"])
+	opponent_stats["def"] = max(0, opponent_stats["def"] - dmg)
 	
 	if (self_stats == battle.current_player_stats):
 		battle.animation_player.play("magic_attack")
