865f5c203934d1618e6eb16a93afd4cfebc4b588
Cierra <cierra362@gmail.com>
2024-11-22 01:19:57 -0500

    WASD controls + hide cursor

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -7,6 +7,7 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	start_game.grab_focus()
 	AudioPlayer.play()
 
