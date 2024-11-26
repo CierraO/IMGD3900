f770e513566d3f5f69f4be97b82c3e3e8e918d19
Cierra <cierra362@gmail.com>
2024-11-26 10:33:43 -0500

    functions to add and equip equipment

--- scripts/PlayerStats.gd
+++ scripts/PlayerStats.gd
@@ -55,6 +55,7 @@ var WEAPON_MAPPINGS = [
 	"def": 0},
 ]
 var weapons_collected = []
+var weapon_equipped = -1
 
 enum HELMETS {WITCH_HAT, KNIGHT_HELM, THIEF_MASK}
 var HELMET_MAPPINGS = [
@@ -72,13 +73,15 @@ var HELMET_MAPPINGS = [
 	"mag": 0,
 	"def": 2},
 	
-	{"name": "Thief's Mask",
+	{"name": "Thief's Hood",
+	"icon": preload("res://assets/thief-hood.png"),
 	"hp": 0,
 	"atk": 2,
 	"mag": 0,
 	"def": 1},
 ]
 var helmets_collected = []
+var helmet_equipped = -1
 
 enum CHESTPIECES {WITCH_CLOAK, KNIGHT_PLATE, THIEF_CLOAK}
 var CHESTPIECE_MAPPINGS = [
@@ -104,28 +107,33 @@ var CHESTPIECE_MAPPINGS = [
 	"def": 2},
 ]
 var chestpieces_collected = []
+var chestpiece_equipped = -1
 
 enum BOOTS {WITCH_BOOTS, KNIGHT_BOOTS, THIEF_BOOTS}
 var BOOT_MAPPINGS = [
 	{"name": "Witch's Boots",
+	"icon": preload("res://assets/witch-boots.png"),
 	"hp": 0,
 	"atk": 0,
 	"mag": 1,
 	"def": 1},
 	
 	{"name": "Knight's Boots",
+	"icon": preload("res://assets/boots-item.png"),
 	"hp": 0,
 	"atk": 0,
 	"mag": 0,
 	"def": 2},
 	
 	{"name": "Thief's Boots",
+	"icon": preload("res://assets/boots-item.png"),
 	"hp": 0,
 	"atk": 2,
 	"mag": 0,
 	"def": 1},
 ]
 var boots_collected = []
+var boots_equipped = -1
 
 ## Player stats
 ## Base stats are the player's stats without equipment or potions,
@@ -141,3 +149,68 @@ var magic_attacks = {
 	"Basic Magic Attack": preload("res://scripts/magic_attacks/basic.gd"),
 	"Lower Enemy Defense": preload("res://scripts/magic_attacks/lower_enemy_def.gd"),
 }
+
+
+## Add the equipment to the player's collection.
+## type is one of ["weapon", "helmet", "chestpiece", "boot"].
+## equipment is an equipment enum value for the given type.
+func collect_equipment(type: String, equipment: int):
+	if not ["weapon", "helmet", "chestpiece", "boot"].has(type):
+		printerr("collect_equipment(): Invalid equipment type!")
+		return
+	
+	if get((type + "s").to_upper()).values().has(equipment):
+		get(type + "s_collected").append(equipment)
+	else:
+		printerr("collect_equipment(): %s doesn't exist!" % type)
+
+
+## type is one of ["weapon", "helmet", "chestpiece", "boot"].
+## collected_equipment is an index into the array of collected equipment of that type,
+## which itself contains indexes into the MAPPINGS aray (or enum values)
+func equip(type: String, collected_equipment: int):
+	if not ["weapon", "helmet", "chestpiece", "boot"].has(type):
+		printerr("equip(): Invalid equipment type!")
+		return
+	
+	var equipments_collected = get(type + "s_collected")
+	var EQUIP_MAPPINGS = get(type.to_upper() + "_MAPPINGS")
+	
+	if (equipments_collected.size() - 1 < collected_equipment
+			or EQUIP_MAPPINGS.size() - 1 < equipments_collected[collected_equipment]):
+		printerr("equip(): %s does not exist!" % type)
+		return
+	
+	var equipped = get(type + "_equipped")
+	if equipped != -1:
+		_remove_equipment_modifiers(equipped)
+	set(type + "_equipped", equipments_collected[collected_equipment])
+	
+	var equipment_dict = EQUIP_MAPPINGS[equipped]
+	_add_equipment_modifiers(equipment_dict)
+
+
+## equipment is a dictionary of equipment attributes
+func _remove_equipment_modifiers(equipment: Dictionary):
+	player_stats["max_hp"] -= equipment["hp"]
+	player_stats["atk"] -= equipment["atk"]
+	player_stats["mag"] -= equipment["mag"]
+	player_stats["def"] -= equipment["def"]
+
+
+## equipment is a dictionary of equipment attributes
+func _add_equipment_modifiers(equipment: Dictionary):
+	player_stats["max_hp"] += equipment["hp"]
+	player_stats["atk"] += equipment["atk"]
+	player_stats["mag"] += equipment["mag"]
+	player_stats["def"] += equipment["def"]
+
+
+func get_equipped_dict(type: String):
+	if not ["weapon", "helmet", "chestpiece", "boot"].has(type):
+		printerr("equip(): Invalid equipment type!")
+		return
+	
+	var equipped = get(type + "_equipped")
+	var EQUIP_MAPPINGS = get(type.to_upper() + "_MAPPINGS")
+	return EQUIP_MAPPINGS[equipped]
